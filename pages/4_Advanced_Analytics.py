26f20547720718e759bfd710050f708fce3dd9a2
Vinya H G <vinyagowda90@gmail.com>
2026-06-04 15:11:33 +0530

    Update 4_Advanced_Analytics.py

--- pages/4_Advanced_Analytics.py
+++ pages/4_Advanced_Analytics.py
@@ -1,16 +1,23 @@
 import streamlit as st
+import pandas as pd
 import plotly.express as px
-from utils.data_loader import load_data
+
+@st.cache_data
+def load_data():
+    return pd.read_csv(
+        "data/diabetes_012_health_indicators_BRFSS2015.csv"
+    )
 
 df = load_data()
 
-st.title("📈 Advanced Analytics")
+st.title("📈 Correlation Analysis")
 
-corr = df.corr()
+corr = df.corr(numeric_only=True)
 
 fig = px.imshow(
     corr,
-    text_auto=True
+    text_auto=False,
+    aspect="auto"
 )
 
 st.plotly_chart(
