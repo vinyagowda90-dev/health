6bb7ff6a92cbf75edd356865d90956332ed03de6
Vinya H G <vinyagowda90@gmail.com>
2026-06-04 15:13:11 +0530

    Update 1_Overview.py

--- pages/1_Overview.py
+++ pages/1_Overview.py
@@ -1,15 +1,24 @@
 import streamlit as st
-from utils.data_loader import load_data
+import pandas as pd
+
+@st.cache_data
+def load_data():
+    return pd.read_csv(
+        "data/diabetes_012_health_indicators_BRFSS2015.csv"
+    )
 
 df = load_data()
 
 st.title("📊 Dataset Overview")
 
-col1,col2,col3,col4 = st.columns(4)
+col1, col2, col3 = st.columns(3)
 
 col1.metric("Rows", df.shape[0])
 col2.metric("Columns", df.shape[1])
 col3.metric("Missing Values", df.isnull().sum().sum())
-col4.metric("Diabetes Cases", df['Diabetes_012'].sum())
 
-st.dataframe(df.head())
+st.subheader("First 10 Rows")
+st.dataframe(df.head(10))
+
+st.subheader("Dataset Information")
+st.write(df.describe())
